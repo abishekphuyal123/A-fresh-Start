94f38609f9ae3a87f592612c3c2d5505bf9f835d
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2023-03-16 23:47:12 +0000

    Add files via upload

---
+++ Day - 1.py
@@ -0,0 +1,9 @@
+# Day - 1 of 100 Days of Python                                             - Following Angela Yu (Udemy)
+# Here, we are going to ask for 2 inputs and generate a band name based on those 2 inputs.
+
+# Let's start. 
+
+print("Welcome to the band name generator.")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's the name of your pet? ")
+print(f"The name of your band could be {city} {pet}.")
